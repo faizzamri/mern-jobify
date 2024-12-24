3fb0d9771482bc6822ee22a173eebe9112632c60
faizzamri <faizzamri21@gmail.com>
2024-12-25 04:16:56 +0800

    setup the port and dotenv

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 import express from "express";
+import morgan from "morgan";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+    res.send("Hello World");
 });
 
-app.listen(5100, () => {
-  console.log("Server is running on port 5100");
+app.post("/", (req, res) => {
+    console.log(req);
+    res.json({ message: "data received", data: req.body });
+});
+
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
 });
