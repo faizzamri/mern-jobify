bacbfb14956e6707c83636e8134a596c639e64e6
faizzamri <faizzamri21@gmail.com>
2025-01-09 01:08:34 +0800

    CRUD operations for job recording

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import * as dotenv from "dotenv";
+import { nanoid } from "nanoid";
+
+let jobs = [
+    { id: nanoid(), company: 'apple', position: 'front-end' },
+    { id: nanoid(), company: 'google', position: 'back-end' },
+]
 
 dotenv.config();
 const app = express();
@@ -19,6 +25,63 @@ app.post("/", (req, res) => {
     console.log(req);
     res.json({ message: "data received", data: req.body });
 });
+
+//Get all jobs
+app.get('/api/v1/jobs', (req, res) => {
+    res.status(200).json({ jobs })
+})
+
+//Create jobs
+app.post('/api/v1/jobs', (req, res) => {
+    const { company, position } = req.body
+    if (!company || !position) {
+        return res.status(400).json({ msg: 'please provide company and position' })
+    }
+    const id = nanoid(10)
+    const job = { id, company, position };
+    jobs.push(job)
+    res.status(200).json({ jobs })
+})
+
+//Get single job
+
+app.get('/api/v1/jobs/:id', (req, res) => {
+    const { id } = req.params;
+    const job = jobs.find((job) => job.id === id)
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` })
+    }
+    res.status(200).json({ job })
+})
+
+//edit job 
+app.patch('/api/v1/jobs/:id', (req, res) => {
+    const { company, position } = req.body
+    if (!company || !position) {
+        return res.status(400).json({ msg: 'please provide company and position' })
+    }
+    const { id } = req.params
+    const job = jobs.find((job) => job.id === id)
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` })
+    }
+    job.company = company
+    job.position = position
+    res.status(200).json({ msg: `Job id: ${id} has been updated`, job })
+})
+
+//delete job
+app.delete('/api/v1/jobs/:id', (req, res) => {
+    const { id } = req.params
+    const job = jobs.find((job) => job.id == id)
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` })
+    }
+    
+    const newJobs = jobs.filter((job) => job.id !== id)
+    jobs = newJobs
+    res.status(200).json({ msg: `Job id: ${id} has been deleted` })
+})
 
 const port = process.env.PORT || 5100;
 
